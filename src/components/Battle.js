7acd0f3fa19ec90b467965da61d8cfa2cac2d001
Brandon <brandonbgrayson@gmail.com>
2021-09-27 20:45:39 -0400

    fiexed bug conditionally rendering PlayerInput

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -2,63 +2,7 @@ import React from "react"
 import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa'
 import PropTypes from "prop-types"
 
-class PlayerInput extends React.Component {
-    constructor(props) {
-        super(props) 
 
-        this.state = {
-            userName: ''
-        }
-
-        this.handleSubmit = this.handleSubmit.bind(this)
-        this.handleChange = this.handleChange.bind(this)
-    }
-
-    handleSubmit (event) {
-
-        event.preventDefault()
-
-        this.props.onSubmit(this.state.userName)
-            
-    }
-
-    handleChange(e) {
-        this.setState({
-            userName: e.target.value
-        })
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit} className="column player">
-                <label htmlFor="username" className="player-label">
-                    {this.props.label}
-                </label>
-                <div className="row player-inputs">
-                    <input 
-                        type="text"
-                        id="username"
-                        className="input-light"
-                        placeholder="github username"
-                        autoComplete="off"
-                        value={this.state.userName}
-                        onChange={this.handleChange}/>
-                    <button
-                     className='btn dark-btn'
-                     type="submit"
-                     disabled={!this.state.userName}>
-                        Submit
-                    </button>
-                </div>
-            </form>
-        )
-    }
-}
-
-PlayerInput.prototypes = {
-    onSubmit: PropTypes.func.isRequired,
-    label: PropTypes.string.isRequired
-}
 
 function Instructions () {
     return (
@@ -82,44 +26,105 @@ function Instructions () {
         </ol>
       </div>
     )
-}
-
-export default class Battle extends React.Component {
+  }
+  
+  class PlayerInput extends React.Component {
     constructor(props) {
-        super(props)
-
-        this.state = {
-            playerOne: null,
-            playerTwo: null
-        }
-        this.handleSubmit = this.handleSubmit.bind(this)
+      super(props)
+  
+      this.state = {
+        username: ''
+      }
+  
+      this.handleSubmit = this.handleSubmit.bind(this)
+      this.handleChange = this.handleChange.bind(this)
     }
-    handleSubmit(id, player) {
-        this.setState({
-            [id]: player
-        })
+    handleSubmit(event) {
+      event.preventDefault()
+  
+      this.props.onSubmit(this.state.username)
+    }
+    handleChange(event) {
+      this.setState({
+        username: event.target.value
+      })
     }
     render() {
-        const {playerOne, playerTwo} = this.state
-        return (
-          <React.Fragment>
-            <Instructions />
-            <div className="players-container">
-                <h1 className="center-text header-lg"> Players</h1>
-                <div className="row space-around">
-                    {playerOne === null && (
-                        <PlayerInput 
-                         label="Player One"
-                         onSubmit={(player) => this.handleSubmit("PlayerOne", player)} />
-                    )}
-                    {playerTwo === null && (
-                        <PlayerInput 
-                         label="Player Two"
-                         onSubmit={(player) => this.handleSubmit("PlayerTwo", player)} />
-                    )}
-                </div>
-            </div>
-          </React.Fragment>
-        )
+      return (
+        <form className='column player' onSubmit={this.handleSubmit}>
+          <label htmlFor='username' className='player-label'>
+            {this.props.label}
+          </label>
+          <div className='row player-inputs'>
+            <input
+              type='text'
+              id='username'
+              className='input-light'
+              placeholder='github username'
+              autoComplete='off'
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+            <button
+              className='btn dark-btn'
+              type='submit'
+              disabled={!this.state.username}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      )
     }
-}
+  }
+  
+  PlayerInput.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired
+  }
+  
+  export default class Battle extends React.Component {
+    constructor(props) {
+      super(props)
+  
+      this.state = {
+        playerOne: null,
+        playerTwo: null,
+      }
+  
+      this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleSubmit(id, player) {
+      this.setState({
+        [id]: player
+      })
+    }
+    render() {
+      const { playerOne, playerTwo } = this.state
+  
+      return (
+        <React.Fragment>
+          <Instructions />
+  
+          <div className='players-container'>
+            <h1 className='center-text header-lg'>Players</h1>
+            <div className='row space-around'>
+              {playerOne === null && (
+                <PlayerInput
+                  label='Player One'
+                  onSubmit={(player) => this.handleSubmit('playerOne', player)}
+                />
+              )}
+  
+              {playerTwo === null && (
+                <PlayerInput
+                  label='Player Two'
+                  onSubmit={(player) => this.handleSubmit('playerTwo', player)}
+                />
+              )}
+            </div>
+          </div>
+        </React.Fragment>
+      )
+    }
+  }
