97b3efe8c6e428311ec56901391fbd70da8a5254
Brandon <brandonbgrayson@gmail.com>
2021-09-29 21:54:33 -0400

    styled PlayerPreview component, able to render PlayerPreview or PlayerInput component conditionally

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa'
+import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa'
 import PropTypes from "prop-types"
 
 
@@ -77,6 +77,38 @@ function Instructions () {
       )
     }
   }
+
+  function PlayerPreview({ username, onReset, label}) {
+      return (
+        <div className='column player'>
+        <h3 className='player-label'>{label}</h3>
+        <div className='row bg-light'>
+          <div className='player-info'>
+            <img
+              className='avatar-small'
+              src={`https://github.com/${username}.png?size=200`}
+              alt={`Avatar for ${username}`}
+            />
+            <a
+              href={`https://github.com/${username}`}
+              className='link'>
+                {username}
+            </a>
+          </div>
+          <button className='btn-clear flex-center' onClick={onReset}>
+            <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
+          </button>
+        </div>
+      </div>
+      )
+
+  }
+
+  PlayerPreview.propTypes = {
+      username: PropTypes.string.isRequired,
+      onReset: PropTypes.func.isRequired,
+      label: PropTypes.string.isRequired
+  }
   
   PlayerInput.propTypes = {
     onSubmit: PropTypes.func.isRequired,
@@ -93,11 +125,18 @@ function Instructions () {
       }
   
       this.handleSubmit = this.handleSubmit.bind(this)
+      this.handleReset = this.handleReset.bind(this)
     }
     handleSubmit(id, player) {
       this.setState({
         [id]: player
       })
+    }
+
+    handleReset(id) {
+        this.setState({
+            [id] : null
+        })
     }
     render() {
       const { playerOne, playerTwo } = this.state
@@ -109,19 +148,28 @@ function Instructions () {
           <div className='players-container'>
             <h1 className='center-text header-lg'>Players</h1>
             <div className='row space-around'>
-              {playerOne === null && (
+              {playerOne === null
+                ?
                 <PlayerInput
                   label='Player One'
                   onSubmit={(player) => this.handleSubmit('playerOne', player)}
                 />
-              )}
+                :
+                <PlayerPreview username={playerOne} label="Player One" onReset={() => this.handleReset("playerOne")}/>
+              }
   
-              {playerTwo === null && (
+              {playerTwo === null
+              ?
                 <PlayerInput
                   label='Player Two'
                   onSubmit={(player) => this.handleSubmit('playerTwo', player)}
                 />
-              )}
+              :
+                <PlayerPreview
+                 username={playerTwo}
+                 label="Player Two"
+                 onReset={() => this.handleReset("playerTwo")}/>
+              }
             </div>
           </div>
         </React.Fragment>
