76ab4dd2ac51e5f4d9505c52c16ca4b1866225f2
Brandon <brandonbgrayson@gmail.com>
2021-10-08 20:27:46 -0400

    added onMouseOver and OnmouseOut hovering effects to location and Company

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Battle from "./components/Battle"
 function App() {
   return (
     <div className="container">
-      <Popular />
+      <Battle />
     </div>
   );
 }
--- src/components/Results.js
+++ src/components/Results.js
@@ -5,35 +5,97 @@ import Card from "./Card"
 import Proptypes from "prop-types"
 import Loading from "./Loading"
 
-function ProfileList ({profile}) {
-  return (
-    <ul className='card-list'>
-    <li>
-      <FaUser color='rgb(239, 115, 115)' size={22} />
-      {profile.name}
-    </li>
-    {profile.location && (
+const styles = {
+  container: {
+    position: 'relative',
+    display: 'flex'
+  },
+  tooltip: {
+    boxSizing: 'border-box',
+    position: 'absolute',
+    width: '160px',
+    bottom: '100%',
+    left: '50%',
+    marginLeft: '-80px',
+    borderRadius: '3px',
+    backgroundColor: 'hsla(0, 0%, 20%, 0.9)',
+    padding: '7px',
+    marginBottom: '5px',
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: '14px',
+  }
+}
+
+
+class ProfileList extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hoveringLocation: false,
+      hoveringCompany: false
+    }
+    this.mouseOver = this.mouseOver.bind(this)
+    this.mouseOut = this.mouseOut.bind(this)
+  }
+
+  mouseOver(id) {
+    this.setState({
+      [id]: true
+    })
+  }
+
+  mouseOut(id) {
+    this.setState({
+      [id]: false
+    })
+  }
+
+  render () {
+
+    const { profile } = this.props
+    const { hoveringLocation, hoveringCompany} = this.state
+    return (
+      <ul className='card-list'>
       <li>
-        <FaCompass color='rgb(144, 115, 255)' size={22} />
-        {profile.location}
+        <FaUser color='rgb(239, 115, 115)' size={22} />
+        {profile.name}
       </li>
-    )}
-    {profile.company && (
+      {profile.location && (
+        <li 
+         onMouseOver={() => this.mouseOver('hoveringLocation')}
+         onMouseOut={() => this.mouseOut('hoveringLocation')}
+         style={styles.container}
+         >
+          {hoveringLocation === true && <div style={styles.tooltip}>Users Location</div>}
+          <FaCompass color='rgb(144, 115, 255)' size={22} />
+          {profile.location}
+        </li>
+      )}
+      {profile.company && (
+        <li
+         onMouseOver={() => this.mouseOver('hoveringCompany')}
+         onMouseOut={() => this.mouseOut('hoveringCompany')}
+         style={styles.container}
+         >
+          { hoveringCompany === true && <div style={styles.tooltip}>Users Company</div>}
+          <FaBriefcase color='#795548' size={22} />
+          {profile.company}
+        </li>
+      )}
       <li>
-        <FaBriefcase color='#795548' size={22} />
-        {profile.company}
+        <FaUsers color='rgb(129, 195, 245)' size={22} />
+        {profile.followers.toLocaleString()} followers
       </li>
-    )}
-    <li>
-      <FaUsers color='rgb(129, 195, 245)' size={22} />
-      {profile.followers.toLocaleString()} followers
-    </li>
-    <li>
-      <FaUserFriends color='rgb(64, 183, 95)' size={22} />
-      {profile.following.toLocaleString()} following
-    </li>
-  </ul>
-  )
+      <li>
+        <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+        {profile.following.toLocaleString()} following
+      </li>
+    </ul>
+    )
+  }
 }
 
 ProfileList.protoTypes = {
