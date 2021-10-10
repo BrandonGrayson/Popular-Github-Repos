d638e026480195e6be97d6612a2c6d275bfe2c09
Brandon <brandonbgrayson@gmail.com>
2021-10-10 19:43:57 -0400

    created a ThemeConsumer and ThemeProvider from createContext to pass the theme to any component in the tree

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import './App.css';
+import React from "react"
 import Popular from "./components/Popular"
 import "../src/index.css"
 import Battle from "./components/Battle"
+import { ThemeProvider } from "../src/contexts/theme"
 
-function App() {
-  return (
-    <div className="container">
-      <Battle />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
+    }
+  }
+  render () {
+    return (
+      <ThemeProvider value={this.state}>
+        <div className="container">
+          <Battle />
+        </div>
+      </ThemeProvider>
+    )
+  }
+
 }
 
 export default App;
