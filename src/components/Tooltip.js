fe2f0dfca35d2bc63f37e98d069f3519e2d17caf
Brandon <brandonbgrayson@gmail.com>
2021-10-10 11:09:54 -0400

    created Hover component to track the state of hovering throughout app

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,43 +1,48 @@
 import React from "react"
 import PropTypes from "prop-types"
-import withHover from "./withHover"
+import Hover from "./Hover"
+
 
 const styles = {
     container: {
-      position: 'relative',
-      display: 'flex'
+        position: 'relative',
+        display: 'flex'
     },
     tooltip: {
-      boxSizing: 'border-box',
-      position: 'absolute',
-      width: '160px',
-      bottom: '100%',
-      left: '50%',
-      marginLeft: '-80px',
-      borderRadius: '3px',
-      backgroundColor: 'hsla(0, 0%, 20%, 0.9)',
-      padding: '7px',
-      marginBottom: '5px',
-      color: '#fff',
-      textAlign: 'center',
-      fontSize: '14px',
+        boxSizing: 'border-box',
+        position: 'absolute',
+        width: '160px',
+        bottom: '100%',
+        left: '50%',
+        marginLeft: '-80px',
+        borderRadius: '3px',
+        backgroundColor: 'hsla(0, 0%, 20%, 0.9)',
+        padding: '7px',
+        marginBottom: '5px',
+        color: '#fff',
+        textAlign: 'center',
+        fontSize: '14px',
     }
-  }
+}
 
-function Tooltip({ text, children, hovering}) {
-    return(
-        <div
-         style={styles.container}
-         >
-            { hovering === true && <div style={styles.tooltip}>{text}</div>}
-            {children}
-        </div>
+function Tooltip({ text, children }) {
+    return (
+        <Hover>
+            { (hovering) => (
+                <div
+                    style={styles.container}
+                >
+                    { hovering === true && <div style={styles.tooltip}>{text}</div>}
+                    {children}
+                </div>
+            )}
+        </Hover>
+
     )
 }
 
 Tooltip.propTypes = {
     text: PropTypes.string.isRequired,
-    hovering: PropTypes.bool.isRequired
 }
 
-export default withHover(Tooltip)
+export default Tooltip
