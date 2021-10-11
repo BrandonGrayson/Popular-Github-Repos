7b2f563738a303d9de6ad2ed3df7ba61c3d892fa
Brandon <brandonbgrayson@gmail.com>
2021-10-11 19:16:27 -0400

    used themeconsumer to update theme inside card, battle and popular components

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -91,26 +91,31 @@ class PlayerInput extends React.Component {
 
 function PlayerPreview({ username, onReset, label }) {
   return (
-    <div className='column player'>
-      <h3 className='player-label'>{label}</h3>
-      <div className='row bg-light'>
-        <div className='player-info'>
-          <img
-            className='avatar-small'
-            src={`https://github.com/${username}.png?size=200`}
-            alt={`Avatar for ${username}`}
-          />
-          <a
-            href={`https://github.com/${username}`}
-            className='link'>
-            {username}
-          </a>
+    <ThemeConsumer>
+      {({ theme }) => (
+        <div className='column player'>
+          <h3 className='player-label'>{label}</h3>
+          <div className={`row bg-${theme}`}>
+            <div className='player-info'>
+              <img
+                className='avatar-small'
+                src={`https://github.com/${username}.png?size=200`}
+                alt={`Avatar for ${username}`}
+              />
+              <a
+                href={`https://github.com/${username}`}
+                className='link'>
+                {username}
+              </a>
+            </div>
+            <button className='btn-clear flex-center' onClick={onReset}>
+              <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
+            </button>
+          </div>
         </div>
-        <button className='btn-clear flex-center' onClick={onReset}>
-          <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
-        </button>
-      </div>
-    </div>
+      )}
+    </ThemeConsumer>
+
   )
 
 }
