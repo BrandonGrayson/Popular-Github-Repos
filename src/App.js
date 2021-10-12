79f53004219d27039e7e53136ffb88c1bd8df38d
Brandon <brandonbgrayson@gmail.com>
2021-10-12 15:54:48 -0400

    installed react router dom and enabled routing between popular and battle components inside of app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "../src/index.css"
 import Battle from "./components/Battle"
 import { ThemeProvider } from "../src/contexts/theme"
 import Nav from "./components/Nav"
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends React.Component {
   constructor(props) {
@@ -21,14 +22,18 @@ class App extends React.Component {
   }
   render() {
     return (
-      <ThemeProvider value={this.state}>
-        <div className={this.state.theme}>
-          <div className="container">
-            <Nav />
-            <Battle />
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className="container">
+              <Nav />
+              <Route exact path='/' component={Popular}></Route>
+              <Route path='/battle' component={Battle}></Route>
+            </div>
           </div>
-        </div>
-      </ThemeProvider>
+        </ThemeProvider>
+      </Router>
+
     )
   }
 
