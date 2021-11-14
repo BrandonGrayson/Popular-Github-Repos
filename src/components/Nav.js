2fda67dc225b6a99faa60fd221c72888d1b57113
Brandon <brandonbgrayson@gmail.com>
2021-11-13 22:16:23 -0500

    refactored nav and card component to use ThemeContext from React useContext

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,52 +1,51 @@
 import React from "react"
-import { ThemeConsumer } from "../contexts/theme"
+import ThemeContext from "../contexts/theme"
 import { NavLink } from "react-router-dom"
 
 const activeStyle = {
     color: 'rgb(187, 46, 31)'
 }
 
-export default function Nav({toggleTheme}) {
+export default function Nav({ toggleTheme }) {
+    const theme = React.useContext(ThemeContext)
     return (
-        <ThemeConsumer>
-            {(theme) => (
-                <nav className='row space-between'>
-                    <ul className='row nav'>
-                        <li>
-                            <NavLink
-                                to='/'
-                                className='nav-link'
-                                exact
-                                activeStyle={activeStyle}
-                                >Popular
+
+        <nav className='row space-between'>
+            <ul className='row nav'>
+                <li>
+                    <NavLink
+                        to='/'
+                        className='nav-link'
+                        exact
+                        activeStyle={activeStyle}
+                    >Popular
                             </NavLink>
-                        </li>
-                        <li>
-                            <NavLink
-                                to='/battle'
-                                className='nav-link'
-                                activeStyle={activeStyle}
-                                >Battle
+                </li>
+                <li>
+                    <NavLink
+                        to='/battle'
+                        className='nav-link'
+                        activeStyle={activeStyle}
+                    >Battle
                             </NavLink>
-                        </li>
-                        <li>
-                            <NavLink
-                                to='/hopeworks'
-                                className='nav-link'
-                                activeStyle={activeStyle}
-                                >HopeWorks
+                </li>
+                <li>
+                    <NavLink
+                        to='/hopeworks'
+                        className='nav-link'
+                        activeStyle={activeStyle}
+                    >HopeWorks
                             </NavLink>
-                        </li>
-                    </ul>
-                    <button
-                        style={{ fontSize: 30 }}
-                        className='btn-clear'
-                        onClick={toggleTheme}
-                    >
-                        {theme === 'light' ? '🔦' : '💡'}
-                    </button>
-                </nav>
-            )}
-        </ThemeConsumer>
+                </li>
+            </ul>
+            <button
+                style={{ fontSize: 30 }}
+                className='btn-clear'
+                onClick={toggleTheme}
+            >
+                {theme === 'light' ? '🔦' : '💡'}
+            </button>
+        </nav>
+
     )
 }
